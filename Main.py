8e451c12e6e19256b9adb88e3ff21d62b3d6d826
Matthew <matthew.morrison153@gmail.com>
2023-04-06 17:47:00 +1000

    Added comments explaining python code

--- Main.py
+++ Main.py
@@ -5,21 +5,22 @@ import requests
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 import time
-import math
 
-spotify_client_id = '30f5cc799ffc4f1a89a2cdc6f8b0784b'
-spotify_client_secret = 'a6d4c157e9e04410b6bf784c26e7ca68'
 
-class YouTubeApiClient():
+spotify_client_id = '30f5cc799ffc4f1a89a2cdc6f8b0784b' # ID used for Spotify API client
+spotify_client_secret = 'a6d4c157e9e04410b6bf784c26e7ca68' # Secret code used for Spotify API client
+
+class YouTubeApiClient(): #YouTube Client 
     def __init__(self):
-        self.scopes = ['https://www.googleapis.com/auth/youtubepartner', 'https://www.googleapis.com/auth/youtube', 'https://www.googleapis.com/auth/youtube.force-ssl']
-        self.client_secrets_file = 'client_secret_555837191026-lm9sjjlrr1oorltd4sngk8rnvnf9bjkp.apps.googleusercontent.com.json'
-        flow = InstalledAppFlow.from_client_secrets_file(self.client_secrets_file, self.scopes)
+        self.scopes = ['https://www.googleapis.com/auth/youtubepartner', 'https://www.googleapis.com/auth/youtube', 'https://www.googleapis.com/auth/youtube.force-ssl'] # Scopes that determine what data the YouTube API can access
+        self.client_secrets_file = 'client_secret_555837191026-lm9sjjlrr1oorltd4sngk8rnvnf9bjkp.apps.googleusercontent.com.json' # Access .json file that holds client secret data
+        # Initiate OAuth 2.0
+        flow = InstalledAppFlow.from_client_secrets_file(self.client_secrets_file, self.scopes) 
         flow.run_local_server()
         self.cridentials = flow.credentials
         self.youtube_service = build('youtube', 'v3', credentials=self.cridentials)
     
-    def getViedoInfoFromQuery(self, queary): #  Gets top videos on YT from search queary
+    def getViedoInfoFromQuery(self, queary): #  Gets top videos on YouTube from search queary
         request = self.youtube_service.search().list(
         part="snippet",
         q=queary,
@@ -29,7 +30,7 @@ class YouTubeApiClient():
         response = request.execute()
         return response
     
-    def createPlaylist(self, name):
+    def createPlaylist(self, name): # Creates YouTube Playlist with given name on users account
         request = self.youtube_service.playlists().insert(
             part="snippet",
             body={
@@ -41,7 +42,7 @@ class YouTubeApiClient():
         response = request.execute()
         return response
     
-    def addSongToPlaylist(self, video_id, playlist_id):
+    def addSongToPlaylist(self, video_id, playlist_id): # Adds a song to the playlist given
         request = add_video_request=self.youtube_service.playlistItems().insert(
         part="snippet",
         body={
@@ -58,22 +59,23 @@ class YouTubeApiClient():
         return response
         
 
-class SpotifyApiClient():
+class SpotifyApiClient(): # Spotify Client
     def __init__(self): #  Creates spotify OAuth client
-        self.sp_oauth = createSpotifyOAuth()
-        code = request.args.get('code')
-        token_info = self.sp_oauth.get_access_token(code)
+        self.sp_oauth = createSpotifyOAuth() # Initiate OAuth 2.0
+        code = request.args.get('code') # Gets varification code that OAuth 2.0 is complete
+        token_info = self.sp_oauth.get_access_token(code) # Gets Spotify API token infomation using OAuth 2.0 code
+        # Extracts individual tokens from token_info
         self.access_token = token_info['access_token']
         self.refresh_token = token_info['refresh_token']
         self.token_expires = token_info['expires_at']
 
-    def tokenRefresh(self): #  Refreshes token
+    def tokenRefresh(self): #  Refreshes token (called when previous one has expired)
         token_info = self.sp_oauth.refresh_access_token(self.refresh_token)
         self.access_token = token_info['access_token']
         self.refresh_token = token_info['refresh_token']
         self.token_expires = token_info['expires_at']
 
-    def getPlaylistInfo(self, playlist_id):
+    def getPlaylistInfo(self, playlist_id): # Fetches Spotify playlist given its ID and returns its corresponding .json file
         headers = {
             "Authorization" : f"Bearer {self.access_token}"
         }
@@ -111,40 +113,36 @@ class SpotifyApiClient():
 
 
 def main():
+    # Create Spotify and YouTube clients
     spotify_client = SpotifyApiClient()
     youtube_client = YouTubeApiClient()
 
-    spotify_playlist_id = '2Kx3nUV09qY9W1jCbXFPUP?si=bd55d256a4244b9d&pt=811cbf9318080ec1b780d18c1a3864d7'
-    spotify_playlist = spotify_client.getPlaylistInfo(playlist_id=spotify_playlist_id)
-    next_url = spotify_playlist['tracks']['next']
-    playlist_length = spotify_playlist['tracks']['total']
+    spotify_playlist_id = '2Kx3nUV09qY9W1jCbXFPUP?si=bd55d256a4244b9d&pt=811cbf9318080ec1b780d18c1a3864d7' # Playlist ID (Only used for testing)
+    spotify_playlist = spotify_client.getPlaylistInfo(playlist_id=spotify_playlist_id) # Gets playlist using ID
+    next_url = spotify_playlist['tracks']['next'] # Extracts URL for the next page of the playlist (each page only contains at most 100 songs)
+    playlist_length = spotify_playlist['tracks']['total'] # Gets playlist length
 
-    youtube_playlist = youtube_client.createPlaylist('Ambient')
+    youtube_playlist = youtube_client.createPlaylist('Ambient') # Creates playlist on users YouTube account
 
     song_index = 0
     page_number = 0
     
-    if song_index >= 100: #  Is the first song on the next page?
-        spotify_playlist = spotify_client.getNextPlaylistInfo(next_url)
-        next_url = spotify_playlist['next']
-        playlist_length = spotify_playlist['total']
-        page_number += 1
-
-    if playlist_length > 100:
-        while song_index < 100: #  Loop that gets songs from playlist and downloads them
-            song_info = spotify_client.getSongInfoFirstPage(spotify_playlist, song_index - 100 * page_number)
-            video_info = youtube_client.getViedoInfoFromQuery(song_info[0] + " " + song_info[1])
+    if playlist_length > 100: # Goes in to this loop because multiple playlist pages will need to be accessed
+        while song_index < 100: #  Loop that gets songs from playlist and downloads them (only for the first page of the playlist)
+            song_info = spotify_client.getSongInfoFirstPage(spotify_playlist, song_index) # Gets infomation on song in playlist, indexed by song_index
+            video_info = youtube_client.getViedoInfoFromQuery(song_info[0] + " " + song_info[1]) # Search for YouTube video using song name and artist from song_info
             youtube_client.addSongToPlaylist(video_info['items'][0]['id']['videoId'], youtube_playlist['id'])
+            # Check if Spotify token has expired and refresh it if it has
             if spotify_client.token_expires < time.time():
                 spotify_client.tokenRefresh()
 
-            song_index += 1
+            song_index += 1 # Increment song_index to get next song
 
-        while song_index < playlist_length: #  Loop for pages other than 1
+        while song_index < playlist_length: #  Loop for pages of the playlist other than 1
             if song_index % 100 == 0 and next_url != None: #  Gets new page every 100 songs
-                spotify_playlist = spotify_client.getNextPlaylistInfo(next_url)
-                next_url = spotify_playlist.json()['next']
-                playlist_length = spotify_playlist.json()['total']
+                spotify_playlist = spotify_client.getNextPlaylistInfo(next_url) # URL for the next page of the playlist
+                next_url = spotify_playlist.json()['next'] # Prepares new URL for the page after this new one
+                playlist_length = spotify_playlist.json()['total'] # Gets playlist length
                 page_number += 1
 
             song_info = spotify_client.getSongInfoSecondPage(spotify_playlist, song_index - 100 * page_number)
@@ -156,7 +154,7 @@ def main():
             song_index += 1
     else:
         while song_index < playlist_length: #  Loop that gets songs from playlist and downloads them
-            song_info = spotify_client.getSongInfoFirstPage(spotify_playlist, song_index - 100 * page_number)
+            song_info = spotify_client.getSongInfoFirstPage(spotify_playlist, song_index - 100 * page_number) # Gets song information by offsetting the song index by the number of pages used
             video_info = youtube_client.getViedoInfoFromQuery(song_info[0] + " " + song_info[1])
             youtube_client.addSongToPlaylist(video_info['items'][0]['id']['videoId'], youtube_playlist['id'])
             if spotify_client.token_expires < time.time():
